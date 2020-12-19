300c62ca6975379bd1ac9f69b878f0631598f9d4
Tushar Jain <tusharjain0022@gmail.com>
2020-12-19 14:21:49 +0530

    console warnings resolved

--- studentportal/src/components/Navbar/Navbar.js
+++ studentportal/src/components/Navbar/Navbar.js
@@ -1,75 +1,103 @@
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div>
-<ul>
-  <li><a href="#logo">Logo</a></li>
-  <li><a href="#home">Project 3001</a></li>
-  <li><a href="#home">Home</a></li>
-  <li><a href="#about">About</a></li>
-  <div className="dropdown">
-    <button className="dropbtn">Resources 
-      <i className="fa fa-caret-down"></i>
-    </button>
-    <div className="dropdown-content">
-      <div className="dropdown">
-      <button className="dropbtn"> 1st year 
-      <i className="fa fa-caret-down"></i>
-      </button>
-        <div className="dropdown-content">
-          <a href="#">CSE</a>
-          <a href="#">ECE</a>
+      <ul>
+        <li>
+          <Link to="/">Logo</Link>
+        </li>
+        <li>
+          <Link to="/">Project 3001</Link>
+        </li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/">About</Link>
+        </li>
+        <div className="dropdown">
+          <button className="dropbtn">
+            Resources
+            <i className="fa fa-caret-down"></i>
+          </button>
+          <div className="dropdown-content">
+            <div className="dropdown">
+              <button className="dropbtn">
+                {" "}
+                1st year
+                <i className="fa fa-caret-down"></i>
+              </button>
+              <div className="dropdown-content">
+                <Link to="first-year-cse">CSE</Link>
+                <Link to="first-year-ece">ECE</Link>
+              </div>
+            </div>
+            <div className="dropdown">
+              <button className="dropbtn">
+                {" "}
+                2nd year
+                <i className="fa fa-caret-down"></i>
+              </button>
+              <div className="dropdown-content">
+                <Link to="second-year-cse">CSE</Link>
+                <Link to="second-year-ece">ECE</Link>
+              </div>
+            </div>
+            <div className="dropdown">
+              <button className="dropbtn">
+                {" "}
+                3rd year
+                <i className="fa fa-caret-down"></i>
+              </button>
+              <div className="dropdown-content">
+                <Link to="third-year-cse">CSE</Link>
+                <Link to="third-year-ece">ECE</Link>
+              </div>
+            </div>
+            <div className="dropdown">
+              <button className="dropbtn">
+                {" "}
+                4th year
+                <i className="fa fa-caret-down"></i>
+              </button>
+              <div className="dropdown-content">
+                <Link to="fourth-year-cse">CSE</Link>
+                <Link to="fourth-year-ece">ECE</Link>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    <div className="dropdown">
-      <button className="dropbtn"> 2nd year 
-      <i className="fa fa-caret-down"></i>
-      </button>
-        <div className="dropdown-content">
-        <a href="#">CSE</a>
-        <a href="#">ECE</a>
+        <li>
+          <Link to="/blog">Blog</Link>
+        </li>
+        <li>
+          <Link to="/calender">Calender</Link>
+        </li>
+        <div className="dropdown">
+          <button className="dropbtn">
+            {" "}
+            Contact Us
+            <i className="fa fa-caret-down"></i>
+          </button>
+          <div className="dropdown-content">
+            <Link to="/">Call us</Link>
+            <Link to="/">Via E-mail</Link>
+          </div>
         </div>
+        <div className="login">
+          <li>
+            <Link to="/">Login</Link>
+          </li>
+        </div>
+        <div className="signup">
+          <li>
+            <Link to="/">Signup</Link>
+          </li>
+        </div>
+      </ul>
     </div>
-    <div className="dropdown">
-      <button className="dropbtn"> 3rd year 
-      <i className="fa fa-caret-down"></i>
-      </button>
-        <div className="dropdown-content">
-        <a href="#">CSE</a>
-        <a href="#">ECE</a>
-        </div>
-     </div>
-     <div className="dropdown">
-      <button className="dropbtn"> 4th year 
-      <i className="fa fa-caret-down"></i>
-      </button>
-        <div className="dropdown-content">
-        <a href="#">CSE</a>
-        <a href="#">ECE</a>
-        </div>
-     </div> 
-      
-    </div>
-  </div>
-  <li><a href="#blog">Blog</a></li>
-  <li><a href="#calender">Calender</a></li>
-  <div className="dropdown">
-      <button className="dropbtn"> Contact Us 
-      <i className="fa fa-caret-down"></i>
-      </button>
-        <div className="dropdown-content">
-          <a href="#">Call us</a>
-          <a href="#">Via E-mail</a>
-        </div>
-   </div>
-   <div className="login"><li><a href="#about">Login</a></li></div>
-  <div className="signup"><li><a href="#about">Signup</a></li></div>
-</ul>
-</div>
   );
 }
 export default Navbar;
-
-
-
