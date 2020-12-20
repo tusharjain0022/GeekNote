23800d7f2f154eec893b30a2c8ecefcfa5144ae4
Tushar Jain <tusharjain0022@gmail.com>
2020-12-20 16:55:03 +0530

    Footer updated

--- studentportal/src/components/Footer/Footer.js
+++ studentportal/src/components/Footer/Footer.js
@@ -1,77 +1,120 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 import "./Footer.css";
-import Form from 'react-bootstrap/Form';
-import youtube from './Youtube.svg';
-import instagram from './Instagram.svg';
-import twitter from './Twitter.svg';
-import dribble from './Dribbble.svg';
+import Form from "react-bootstrap/Form";
+import youtube from "./Youtube.svg";
+import instagram from "./Instagram.svg";
+import twitter from "./Twitter.svg";
+import dribble from "./Dribbble.svg";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-      <div className="main-footer">
-          <div className="container">
-              <div className="row">
-                  {/* Column1 */}
-                  <div className="col-4">
-                      <div className="Option"><h4>Project 3001</h4></div>
-                      <div className="Contents">
-                          <ui className="list-unstyled">
-                              <li>Copyright&copy;3001</li>
-                              <li>All rights reserved</li>
-                          </ui>
-                      </div>
-                      <div className="Links">
-                          <a href ="#"><img src={instagram} alt="Instagram"></img></a>
-                          <a href ='#'><img src={dribble} alt="Dribbble"></img></a>
-                          <a href ="#"><img src={youtube} alt="Youtube"></img></a>
-                          <a href ="#"><img src={twitter} alt="Twitter"></img></a>
-                      </div>
-                  </div>
-                  {/*Column2*/}
-                  <div className="col-2">
-                      <div className="Option"><h4>Links</h4></div>
-                      <div className="Contents">
-                          <ui className="list-unstyled">
-                          <li><a href="#">About Us</a></li>
-                          <li><a href="#">Blog</a></li>
-                          <li><a href="#">Resources</a></li>
-                          <li><a href="#">Home</a></li>
-                          <li><a href="#">Calendar</a></li>
-                      </ui>
-                      </div>
-                  </div>
-                  {/*Column3*/}
-                  <div className="col-3">
-                      <div className="Option"><h4>Support</h4></div>
-                      <div className="Contents">
-                      <ui className="list-unstyled">
-                          <li><a href="#">Contact CR</a></li>
-                          <li><a href="#">Contact Page admins</a></li>
-                          <li><a href="#">Developers</a></li>
-                          <li><a href="#">Terms and Policy</a></li>
-                          <li><a href="#">Login/SignUp</a></li>
-                      </ui>
-                      </div>
-                  </div>
-                  {/*Column4*/}
-                  <div className="col-3">
-                      <div className="sp"><h4>Stay Up to Date</h4></div>
-                      <div className="sp-contents">
-                          <Form>
-                              <Form.Group controlId="formBasicEmail">
-                                  <Form.Control type="email" placeholder="Enter email" />
-                              </Form.Group>
-                          </Form>
-                      </div>
-                      <div className="sp"><h4>Want to Contribute?</h4></div>
-                      <div className="sp-contents">
-                          <div className="text"><a href="https://github.com/tusharjain0022/3001">github.com/tusharjain0022/3001</a></div>
-                      </div>
-                  </div>
-              </div>
+    <div className="main-footer">
+      <div className="container">
+        <div className="row">
+          {/* Column1 */}
+          <div className="col-4">
+            <div className="Option">
+              <h4>Project 3001</h4>
+            </div>
+            <div className="Contents">
+              <ui className="list-unstyled">
+                <li>Copyright&copy;3001</li>
+                <li>All rights reserved</li>
+              </ui>
+            </div>
+            <div className="Links">
+              <Link to="/">
+                <img src={instagram} alt="Instagram"></img>
+              </Link>
+              <Link to="/">
+                <img src={dribble} alt="Dribbble"></img>
+              </Link>
+              <Link to="/">
+                <img src={youtube} alt="Youtube"></img>
+              </Link>
+              <Link to="/">
+                <img src={twitter} alt="Twitter"></img>
+              </Link>
+            </div>
           </div>
+          {/*Column2*/}
+          <div className="col-2">
+            <div className="Option">
+              <h4>Links</h4>
+            </div>
+            <div className="Contents">
+              <ui className="list-unstyled">
+                <li>
+                  <Link to="/">About Us</Link>
+                </li>
+                <li>
+                  <Link to="/blog">Blog</Link>
+                </li>
+                <li>
+                  <Link to="/resources">Resources</Link>
+                </li>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/">Calendar</Link>
+                </li>
+              </ui>
+            </div>
+          </div>
+          {/*Column3*/}
+          <div className="col-3">
+            <div className="Option">
+              <h4>Support</h4>
+            </div>
+            <div className="Contents">
+              <ui className="list-unstyled">
+                <li>
+                  <Link to="/contactus">Contact CR</Link>
+                </li>
+                <li>
+                  <Link to="/contactus">Contact Page admins</Link>
+                </li>
+                <li>
+                  <Link to="/developers">Developers</Link>
+                </li>
+                <li>
+                  <Link to="/terms-and-policy">Terms and Policy</Link>
+                </li>
+                <li>
+                  <Link to="/">Login/SignUp</Link>
+                </li>
+              </ui>
+            </div>
+          </div>
+          {/*Column4*/}
+          <div className="col-3">
+            <div className="sp">
+              <h4>Stay Up to Date</h4>
+            </div>
+            <div className="sp-contents">
+              <Form>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+              </Form>
+            </div>
+            <div className="sp">
+              <h4>Want to Contribute?</h4>
+            </div>
+            <div className="sp-contents">
+              <div className="text">
+                <Link to="https://github.com/tusharjain0022/3001">
+                  github.com/tusharjain0022/3001
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
   );
 }
 export default Footer;
