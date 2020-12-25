fe1e719b9feae87b38fc11f31b6c158e26d89087
Sahil Gupta <sahilgupta.btech.cs19@iiitranchi.ac.in>
2020-12-25 16:05:50 +0530

    Events issue Resolved

--- studentportal/src/components/BranchSection/Calendar/Calendar.js
+++ studentportal/src/components/BranchSection/Calendar/Calendar.js
@@ -7,13 +7,13 @@ function Calendar() {
             <div className="d-block d-sm-none col-12">
 				<h4>Stay up to Date</h4>
                 <iframe title="Important College Dates"
-                        src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FKolkata&amp;src=dGJnY3ZyMmcwbWI3bnYzbDFhYnFsb29mZnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23B39DDB&amp;showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showTz=0&amp;showCalendars=0"
+                        src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FKolkata&amp;src=Y3YyN3JzaHFlYzZ2dXZjZWg0aXRmdXNuZWNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=dGJnY3ZyMmcwbWI3bnYzbDFhYnFsb29mZnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%238E24AA&amp;color=%23B39DDB&amp;showTitle=0&amp;showTabs=0&amp;showTz=0&amp;showPrint=0"
                         width="100%" height="600" frameBorder="0" scrolling="no"/>
             </div>
             <div className="d-none d-sm-block bigScreen">
 				<h2>Stay up to Date</h2>
                 <iframe title="Important College Dates"
-                        src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FKolkata&amp;src=dGJnY3ZyMmcwbWI3bnYzbDFhYnFsb29mZnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23B39DDB&amp;showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showTz=0&amp;showCalendars=0"
+                        src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FKolkata&amp;src=Y3YyN3JzaHFlYzZ2dXZjZWg0aXRmdXNuZWNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=dGJnY3ZyMmcwbWI3bnYzbDFhYnFsb29mZnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%238E24AA&amp;color=%23B39DDB&amp;showTitle=0&amp;showTabs=0&amp;showTz=0&amp;showPrint=0"
                         width="700" height="600" frameBorder="0" scrolling="no"/>
             </div>
         </div>
