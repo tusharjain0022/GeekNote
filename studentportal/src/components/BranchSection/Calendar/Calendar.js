5ad00165425937ab03a744a97f90bf593b6e53d5
Sahil Gupta <sahilgupta.btech.cs19@iiitranchi.ac.in>
2020-12-25 13:35:45 +0530

    Calendar Integration

--- studentportal/src/components/BranchSection/Calendar/Calendar.js
+++ studentportal/src/components/BranchSection/Calendar/Calendar.js
@@ -1,9 +1,13 @@
 import "./Calendar.css";
+import React from "react";
 
 function Calendar() {
 	return (
 		<div className="Calendar">
-			<h1>This is a Calendar.</h1>
+			<h1>Stay up to Date</h1>
+			<iframe title="Calendar for Important institute dates"
+				src="https://calendar.google.com/calendar/embed?src=cv27rshqec6vuvceh4itfusnec%40group.calendar.google.com&ctz=Asia%2FKolkata"
+				width="800" height="600" frameBorder="0" scrolling="no"/>
 		</div>
 	);
 }
