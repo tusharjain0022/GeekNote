e6b1ec407129d165f85d88ef390a5d2ed1be1ef2
Abhay Raj <abhayrajsingh1707@gmail.com>
2020-12-26 18:40:47 +0530

    Carousel for Blog Page (#53)
    * update in login modal
    
    * final change
    
    * change in readme and contribute.m
    
    * carousel for blog page
    
    * Text modified
    
    * remove warnings
    
    * issue resolved

--- studentportal/src/components/Blog/Blog.js
+++ studentportal/src/components/Blog/Blog.js
@@ -1,21 +1,27 @@
 import "./Blog.css";
-import Carousal from "./Carousal/Carousal";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CarouselModified from "./CarouselModified";
+import Carousel_data from "../../data/carouselData.json"
 import AdminBlog from "./Admin/Admin";
 import SearchStats from "./SearchAndStats/SearchStats";
 import CRBlog from "./CR/CR";
 import StudentBlog from "./Students/Student";
 import BlogFooter from "./BlogFooter/BlogFooter";
 
+
 function Blog() {
-	return (
-		<div className="blog">
-			<Carousal />
+  return (
+    <div className="blog">
+       <CarouselModified data={Carousel_data}/>
 			<AdminBlog />
 			<SearchStats />
 			<CRBlog />
 			<StudentBlog />
 			<BlogFooter />
-		</div>
-	);
-}
+    </div>
+
+    
+);
+  
+  }
 export default Blog;
---
+++ studentportal/src/components/Blog/CarouselModified/index.js
@@ -0,0 +1,81 @@
+import { Carousel } from "react-responsive-carousel";
+import CarouselCard from "../CarouselCard";
+import "./index.css";
+import "react-responsive-carousel/lib/styles/carousel.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+// import NavigateBeforeOutlinedIcon from "@material-ui/icons/NavigateBeforeOutlined";
+// import NavigateNextOutlinedIcon from "@material-ui/icons/NavigateNextOutlined";
+
+
+
+const CarouselModified = (props) => {
+
+  let categroisedData = props.data;
+
+  const getConfigurableProps = () => ({
+    showArrows: true,
+    showStatus: false,
+    showIndicators: true,
+    infiniteLoop: true,
+    showThumbs: false,
+    useKeyboardArrows: true,
+    autoPlay: true,
+    stopOnHover: true,
+    swipeable: true,
+    dynamicHeight: false,
+    emulateTouch: true,
+  });
+
+  const arrowStyles = {
+    position: "absolute",
+    zIndex: 2,
+    top: "calc(25%)",
+    width: 30,
+    height: 30,
+    cursor: "pointer",
+    background: "#fff",
+    color: "#000000",
+  };
+
+  return (
+    <>
+      <Carousel
+        {...getConfigurableProps()}
+        renderArrowPrev={(onClickHandler, hasPrev, label) =>
+          hasPrev && (
+            <button
+              type="button"
+              onClick={onClickHandler}
+              title={label}
+              style={{ ...arrowStyles, left: 15 }}
+            >
+              {/* <NavigateBeforeOutlinedIcon /> */}
+            </button>
+          )
+        }
+        renderArrowNext={(onClickHandler, hasNext, label) =>
+          hasNext && (
+            <button
+              type="button"
+              onClick={onClickHandler}
+              title={label}
+              style={{ ...arrowStyles, right: 15 }}
+            >
+              {/* <NavigateNextOutlinedIcon /> */}
+            </button>
+          )
+        }
+      >
+        {categroisedData.map((groupedItem, index) => {
+          return (
+            <div className="carousel-slider " key={index}>
+              <CarouselCard data={groupedItem} />
+            </div>
+          );
+        })}
+      </Carousel>
+    </>
+  );
+};
+
+export default CarouselModified;
