f48de96379ae59bf78031ad17e69f42fecca1237
Sourav <saurav.saw2@gmail.com>
2020-12-27 12:07:07 +0530

    Resources added

--- studentportal/src/components/BranchSection/Resources/Resources.js
+++ studentportal/src/components/BranchSection/Resources/Resources.js
@@ -1,10 +1,39 @@
 import "./Resources.css";
+import branches from "./branches.json"
 
-function Resources() {
-	return (
-		<div className="Resources">
-			<h1>This is a Resources section</h1>
-		</div>
-	);
+const Branch = ({year, dep, theory, prac, credits}) => {
+    return(
+        <div className="card" >
+          <div className="cardContent">
+            <div>
+              <h3>{year}</h3>
+              <h2>{dep}</h2>
+              </div>
+            <ul>
+                <li>Theory Subjects: {theory}</li>
+                <li>Practical Subjects: {prac}</li>
+                <li>Credits: {credits}</li>
+            </ul>
+            <a href="location" className="stretched-link" />
+          </div>
+        </div>
+    )
+}
+function Resources(){
+    return (
+        <section>
+          <h1 className="headdd" >RESOURCES</h1>
+          
+          <div class="allcards">
+            <div className="cards">
+              {
+                  branches.map((branch) => {
+                      return <Branch key={branch.id} {...branch} />
+                  })
+              }
+              </div>
+          </div>
+        </section>
+    )
 }
 export default Resources;
