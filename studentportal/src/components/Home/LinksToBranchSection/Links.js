23d2d3592f81a6b5a3604f0b610d20503195563a
Tushar Jain <tusharjain0022@gmail.com>
2021-01-07 00:12:02 +0530

    fix contact-us component

--- studentportal/src/components/Home/LinksToBranchSection/Links.js
+++ studentportal/src/components/Home/LinksToBranchSection/Links.js
@@ -1,42 +1,39 @@
 import "./Links.css";
-import branches from "../../../data/branches.json"
+import branches from "../../../data/branches.json";
 
-const Branch = ({year, dep, theory, prac, credits}) => {
-    return(
-        <div className="card" >
-          <div className="cardContent">
-            <div>
-              <h3>{year}</h3>
-              <h2>{dep}</h2>
-              </div>
-            <ul className="academics">
-                <li>Theory Subjects: {theory}</li>
-                <li>Practical Subjects: {prac}</li>
-                <li>
-                <a href="location" className="stretched-link" >
-                  Credits: {credits}
-                </a>
-                </li>
-            </ul>
-          </div>
+const Branch = ({ year, dep, theory, prac, credits }) => {
+  return (
+    <div className="card resource-card">
+      <div className="cardContent">
+        <div>
+          <h3 className="year">{year}</h3>
+          <h2>{dep}</h2>
         </div>
-    )
-}
-function Links(){
-    return (
-        <section>
-          <h1 className="headdd" >RESOURCES</h1>
-          
-          <div className="allcards">
-            <div className="cards">
-              {
-                  branches.map((branch) => {
-                      return <Branch key={branch.id} {...branch} />
-                  })
-              }
-              </div>
-          </div>
-        </section>
-    )
+        <ul className="academics list-style-none">
+          <li>Theory Subjects: {theory}</li>
+          <li>Practical Subjects: {prac}</li>
+          <li>
+            <a href="location" className="stretched-link">
+              Credits: {credits}
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+function Links() {
+  return (
+    <section className="resource-section">
+      <h1 className="main-heading">RESOURCES</h1>
+      <div className="allcards">
+        <div className="cards">
+          {branches.map((branch) => {
+            return <Branch key={branch.id} {...branch} />;
+          })}
+        </div>
+      </div>
+    </section>
+  );
 }
 export default Links;
