08b21f638c240163d6ed7e5b14c67de08638e1cf
Abhay Raj <abhayrajsingh1707@gmail.com>
2021-01-06 14:19:45 +0530

    remov warnings

--- studentportal/src/components/BranchSection/BranchSection.js
+++ studentportal/src/components/BranchSection/BranchSection.js
@@ -1,6 +1,6 @@
 import "./BranchSection.css";
 import Landing from "./LandingSection/Landing";
-import Resources from "./Resources/Resources";
+// import Resources from "./Resources/Resources";
 import LinkedBlog from "./LinkedBlog/LinkedBlog";
 import Calendar from "./Calendar/Calendar";
 import CRDetails from "./CRDetails/CRDetails";
