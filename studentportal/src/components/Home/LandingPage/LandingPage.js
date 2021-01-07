379c52243f3c1ec20ad4f9e171aa25b39c7331f3
Tushar Jain <tusharjain0022@gmail.com>
2021-01-07 21:05:18 +0530

    Landing page for desktop

--- studentportal/src/components/Home/LandingPage/LandingPage.js
+++ studentportal/src/components/Home/LandingPage/LandingPage.js
@@ -1,53 +1,35 @@
 import "./LandingPage.css";
+import landing from "./landing.svg";
+// import SlackButton from "./slackButton";
 
 function LandingPage() {
   return (
     <div>
-      <h1>This is a LandingPage Section</h1>
-      <div>
-        <a className="slack-button" href="#invite" target="_blank">
-          <div className="svg-container">
-            <svg
-              width="64"
-              height="64"
-              viewBox="0 0 64 64"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g clipPath="url(#clip0)">
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M23.465 0C19.9272 0.00260958 17.064 2.86793 17.0666 6.3987C17.064 9.92946 19.9298 12.7948 23.4676 12.7974H29.8687V6.4013C29.8713 2.87054 27.0055 0.00521916 23.465 0C23.4676 0 23.4676 0 23.465 0V0ZM23.465 17.0667H6.40081C2.86298 17.0693 -0.0028518 19.9346 -0.000236995 23.4654C-0.00546661 26.9961 2.86036 29.8614 6.39819 29.8667H23.465C27.0029 29.8641 29.8687 26.9987 29.8661 23.468C29.8687 19.9346 27.0029 17.0693 23.465 17.0667V17.0667Z"
-                  fill="#36C5F0"
-                ></path>
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M64 23.4654C64.0026 19.9346 61.1368 17.0693 57.599 17.0667C54.0611 17.0693 51.1953 19.9346 51.1979 23.4654V29.8667H57.599C61.1368 29.8641 64.0026 26.9987 64 23.4654ZM46.9332 23.4654V6.3987C46.9358 2.87054 44.0726 0.00521916 40.5347 0C36.9969 0.00260958 34.1311 2.86793 34.1337 6.3987V23.4654C34.1284 26.9961 36.9943 29.8614 40.5321 29.8667C44.0699 29.8641 46.9358 26.9987 46.9332 23.4654Z"
-                  fill="#2EB67D"
-                ></path>
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M40.5321 63.9997C44.0699 63.9971 46.9358 61.1317 46.9331 57.601C46.9358 54.0702 44.0699 51.2049 40.5321 51.2023H34.131V57.601C34.1284 61.1291 36.9943 63.9944 40.5321 63.9997ZM40.5321 46.9304H57.599C61.1368 46.9278 64.0026 44.0624 64 40.5317C64.0052 37.0009 61.1394 34.1356 57.6016 34.1304H40.5347C36.9969 34.133 34.131 36.9983 34.1337 40.5291C34.131 44.0624 36.9943 46.9278 40.5321 46.9304V46.9304Z"
-                  fill="#ECB22E"
-                ></path>
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M1.78791e-06 40.532C-0.00261302 44.0628 2.86322 46.9281 6.40105 46.9307C9.93889 46.9281 12.8047 44.0628 12.8021 40.532V34.1333H6.40105C2.86322 34.1359 -0.00261302 37.0012 1.78791e-06 40.532ZM17.0669 40.532V57.5987C17.0616 61.1295 19.9275 63.9948 23.4653 64C27.0031 63.9974 29.8689 61.1321 29.8663 57.6013V40.5372C29.8716 37.0065 27.0057 34.1411 23.4679 34.1359C19.9274 34.1359 17.0642 37.0012 17.0669 40.532C17.0669 40.5346 17.0669 40.532 17.0669 40.532Z"
-                  fill="#E01E5A"
-                ></path>
-              </g>
-              <defs>
-                <clipPath id="clip0">
-                  <rect width="64" height="64" fill="white"></rect>
-                </clipPath>
-              </defs>
-            </svg>
+      {/* <SlackButton /> */}
+      <div className="container l">
+        <div className="row landing_page">
+          <div className="text-intro">
+            <div className="text-box align-middle">
+              <h1 className="main-head">
+                Bring everyone <br />
+                on board.
+              </h1>
+              <p className="small-head">
+                <span>
+                  Organised Notes and Study Material at one place. Separate
+                  discussion and blog Space, Year-wise and Branch-wise seperated
+                  space.
+                </span>
+              </p>
+              <button type="button " className="btn explore-button">
+                Explore
+              </button>
+            </div>
           </div>
-        </a>
+          <div className="pic-div">
+            <img src={landing} className="pic" alt="default landing page"></img>
+          </div>
+        </div>
       </div>
     </div>
   );
