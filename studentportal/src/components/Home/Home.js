8ffe294e7f1267990e2dbe6eec76b9bd36119037
Tushar Jain <tusharjain0022@gmail.com>
2021-01-17 23:28:28 +0530

    minor update

--- studentportal/src/components/Home/Home.js
+++ studentportal/src/components/Home/Home.js
@@ -1,8 +1,8 @@
 import "./Home.css";
-// import About from "./About/About";
+import About from "./About/About";
 import Features from "./Features/Features";
 import LandingPage from "./LandingPage/LandingPage";
-// import Links from "./LinksToBranchSection/Links";
+import Links from "./LinksToBranchSection/Links";
 import Contact from "./Contact/Contact";
 
 function Home() {
