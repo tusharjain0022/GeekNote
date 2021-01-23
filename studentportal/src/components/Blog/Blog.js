5ef09d0ec75493f31796e3142bb9745d566b2552
sandesh-08 <sandesh.btech.ec19@iiitranchi.ac.in>
2021-01-23 18:44:30 +0530

    updated search and stats section

--- studentportal/src/components/Blog/Blog.js
+++ studentportal/src/components/Blog/Blog.js
@@ -6,32 +6,40 @@ import AdminBlog from "./Admin/Admin";
 import SearchStats from "./SearchAndStats/SearchStats";
 import StudentBlog from "./Students/Student";
 import BlogFooter from "./BlogFooter/BlogFooter";
+import { useState } from 'react';
 
 function Blog() {
+
+  const [global_tag, set_global_tag] = useState('All');
+
+  const transfer_data_tag = (new_tag) => {
+    set_global_tag(new_tag);
+  }
+
   return (
     <div className="blog">
       <CarouselModified data={Carousel_data} />
       <div className="row">
         <div className="col-8 d-none d-xl-block admin_container">
           <div className="col-12 styleAdmin">
-            <AdminBlog tag={"all"} />
+            <AdminBlog tag={global_tag} />
           </div>
         </div>
         <div className="col-4 d-none d-xl-block search_stats">
           <div className="styleSearch">
-            <SearchStats />
+            <SearchStats transfer_data_tag={transfer_data_tag} />
           </div>
         </div>
       </div>
       <div className="mobileContainer">
         <div className=" col-12 d-xl-none row">
-          <SearchStats />
+          <SearchStats transfer_data_tag={transfer_data_tag} />
         </div>
         <div className="col-12 d-xl-none row">
-          <AdminBlog tag={"all"} />
+          <AdminBlog tag={global_tag} />
         </div>
       </div>
-      <StudentBlog tag={"all"} />
+      <StudentBlog tag={global_tag} />
       <BlogFooter />
     </div>
   );
--- studentportal/src/components/Blog/SearchAndStats/SearchStats.js
+++ studentportal/src/components/Blog/SearchAndStats/SearchStats.js
@@ -2,19 +2,44 @@ import "./SearchStats.css";
 import { Input } from "mdbreact";
 import tagDetails from '../../../data/tag_details.json';
 import { useState } from 'react';
+import { InputGroup, Dropdown } from 'react-bootstrap';
 
-function SearchStats() {
+function SearchStats({ transfer_data_tag }) {
 	const [input, setInput] = useState('');
+
 	return (
 		<div className="container">
 			<div className="row">
 				<div className="SearchBar col-sm-12">
-					<Input className="bar-style"
-						icon="search"
-						value={input}
-						onInput={e => setInput(e.target.value)}
-						placeHolder="Search: Title, Topic, Writer"
-					/>
+					<InputGroup className="mb-3">
+						<Input className="bar-style"
+							icon="search"
+							value={input}
+							onInput={
+								(e) => {
+									setInput(e.target.value);
+									transfer_data_tag(e.target.value);
+								}
+							}
+							placeHolder="Search: Title, Topic, Writer"
+						/>
+						<Dropdown>
+							<Dropdown.Toggle variant="dark" id="dropdown-basic"></Dropdown.Toggle>
+							<Dropdown.Menu>
+								{
+									tagDetails.map((tag_name) => {
+										return <Dropdown.Item variant="dark" onClick={
+											(e) => {
+												setInput(tag_name.tag);
+												transfer_data_tag(tag_name.tag);
+											}
+										}>{tag_name.tag}</Dropdown.Item>
+									})
+								}
+							</Dropdown.Menu>
+						</Dropdown>
+					</InputGroup>
+
 				</div>
 				<div className="for_tags col-sm-12">
 					{
