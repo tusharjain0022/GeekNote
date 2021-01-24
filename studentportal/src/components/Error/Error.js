16840d24f567ce681c56a37ec0fcfc56b33a8d48
Sourav Kumar <56984144+sourav1kumar@users.noreply.github.com>
2021-01-25 00:47:34 +0530

    changed error style (#115)

--- studentportal/src/components/Error/Error.js
+++ studentportal/src/components/Error/Error.js
@@ -3,9 +3,8 @@ import "./Error.css";
 function Error() {
   return (
     <div className="error">
-      <h2>Oops!!!  ERROR</h2>
       <h1>404</h1>
-      <p>The page you're looking for, doesn't exist.</p>
+      <p>This page could not be found.</p>
     </div>
   );
 }
