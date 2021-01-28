9806bb4e229ea265b57f1c6b6d7ff5cadcee0517
Sahil Gupta <sahilgupta.btech.cs19@iiitranchi.ac.in>
2021-01-28 14:39:24 +0530

    Preloader added

---
+++ studentportal/public/preloader.js
@@ -0,0 +1,22 @@
+var loader;
+
+function loadNow(opacity) {
+    if (opacity <= 0) {
+        displayContent();
+    } else {
+        loader.style.opacity = opacity;
+        window.setTimeout(function() {
+            loadNow(opacity - 0.05);
+        }, 50);
+    }
+}
+
+function displayContent() {
+    loader.style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loader = document.getElementById('loader');
+    loadNow(1);
+});
