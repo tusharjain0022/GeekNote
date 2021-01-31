d3edbe90535f999691ee4a92779da6b475beb3b9
Sourav <saurav.saw2@gmail.com>
2021-01-31 17:51:21 +0530

    read branches from json file

--- studentportal/src/App.js
+++ studentportal/src/App.js
@@ -18,62 +18,11 @@ function App() {
           <Switch>
             <Route exact path="/blog" component={Blog} />
             <Route exact path="/" component={Home} />
-            <Route
-              exact
-              path="/web-development"
-              component={() => <BranchSection 
-                                        name="Web Development" 
-                                        intro = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tempor massa. Praesent at lacinia nisi, faucibus finibus mauris." />}
-            />
-            <Route
-              exact
-              path="/app-development"
-              component={() => <BranchSection 
-                                        name="App Development"
-                                        intro = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tempor massa. Praesent at lacinia nisi, faucibus finibus mauris." />}
-            />
-            <Route
-              exact
-              path="/robotics"
-              component={() => <BranchSection 
-                                        name="Robotics and IoT"
-                                        intro = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tempor massa. Praesent at lacinia nisi, faucibus finibus mauris." />}
-            />
-            <Route
-              exact
-              path="/machine-learning"
-              component={() => <BranchSection name="Machine Learning" 
-                                        intro = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tempor massa. Praesent at lacinia nisi, faucibus finibus mauris."/>}
-            />
-            <Route
-              exact
-              path="/competitive-programming"
-              component={() => <BranchSection name="Competitive Programming" 
-                                        intro = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tempor massa. Praesent at lacinia nisi, faucibus finibus mauris."/>}
-            />
-            <Route
-              exact
-              path="/ar-vr"
-              component={() => <BranchSection name="AR/VR" 
-                                        intro = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tempor massa. Praesent at lacinia nisi, faucibus finibus mauris."/>}
-            />
-            <Route
-              exact
-              path="/3d-modelling"
-              component={() => <BranchSection name="3D Modelling" 
-                                        intro = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tempor massa. Praesent at lacinia nisi, faucibus finibus mauris."/>}
-            />
-            <Route
-              exact
-              path="/cloud-computing"
-              component={() => <BranchSection name="Cloud Computing" 
-                                        intro = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tempor massa. Praesent at lacinia nisi, faucibus finibus mauris."/>}
-            />
+            <Route exact path="/:link" children={<BranchSection />} />
             <Route
               exact
               path="/terms-and-policy"
-              component={() => <Documentation name="Terms and Policy" 
-                                        intro = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tempor massa. Praesent at lacinia nisi, faucibus finibus mauris."/>}
+              component={() => <Documentation name="Terms and Policy" />}
             />
             <Route path="/" component={Error} />
           </Switch>
--- studentportal/src/components/BranchSection/BranchSection.js
+++ studentportal/src/components/BranchSection/BranchSection.js
@@ -1,14 +1,28 @@
+import React, {useState, useEffect} from 'react'; 
 import "./BranchSection.css";
+import { useParams } from 'react-router-dom';
+import data from '../../data/branches.json';
+
 import Landing from "./LandingSection/Landing";
 import LinkedBlog from "./LinkedBlog/LinkedBlog";
 import Calendar from "./Calendar/Calendar";
 import CRDetails from "./CRDetails/CRDetails";
 
-function BranchSection(props) {
+
+function BranchSection() {
+  const [branch, setBranch] = useState('Wev dev');
+  const [intro, setIntro] = useState('This is default intro');
+  const {link} = useParams();
+
+  useEffect(() => {
+      const newBranch = data.find((branch) => branch.link === link);
+      setBranch(newBranch.dep);
+      setIntro(newBranch.intro);
+  }, [link]);
   return (
     <div className="branchsection">
-      <Landing name={props.name} intro={props.intro}/>
-      <LinkedBlog name={props.name} />
+      <Landing name={branch} intro={intro}/>
+      <LinkedBlog name={branch} />
       <Calendar />
       <CRDetails />
     </div>
