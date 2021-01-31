77ad1c317cd93a27ef0b8434b11ea72a93c5e1f5
Tushar Jain <tusharjain0022@gmail.com>
2021-01-31 04:07:29 -0800

    Added Modal for Developer, Admins  (#130)
    * developer and admin modal added and favicon added

---
+++ studentportal/src/components/Home/Contact/PageAdmin/PageAdminDetails.js
@@ -0,0 +1,47 @@
+const admins = [
+  {
+    id: 0,
+    name: "Tushar",
+    intro:
+      "lorem ipsum lorem ipsum v lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+    image: "./images/tushar.jpeg",
+    githubLink: "https://google.com",
+    linkedinLink: "https://google.com",
+    mail: "https://google.com",
+    instaLink: "https://google.com",
+  },
+  {
+    id: 1,
+    name: "Tushar",
+    intro:
+      "lorem ipsum lorem ipsum v lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+    image: "./images/tushar.jpeg",
+    githubLink: "https://google.com",
+    linkedinLink: "https://google.com",
+    mail: "https://google.com",
+    instaLink: "https://google.com",
+  },
+  {
+    id: 2,
+    name: "Tushar",
+    intro:
+      "lorem ipsum lorem ipsum v lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+    image: "./images/tushar.jpeg",
+    githubLink: "https://google.com",
+    linkedinLink: "https://google.com",
+    mail: "https://google.com",
+    instaLink: "https://google.com",
+  },
+  {
+    id: 3,
+    name: "Tushar",
+    intro:
+      "lorem ipsum lorem ipsum v lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+    image: "./images/tushar.jpeg",
+    githubLink: "https://google.com",
+    linkedinLink: "https://google.com",
+    mail: "https://google.com",
+    instaLink: "https://google.com",
+  },
+];
+export default admins;
