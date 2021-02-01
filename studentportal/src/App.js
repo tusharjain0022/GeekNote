9c6b324f767bf67c333335abd7b3a5989bcefd9a
sandesh-08 <sandesh.btech.ec19@iiitranchi.ac.in>
2021-02-01 17:30:41 +0530

    dynamic routing

--- studentportal/src/App.js
+++ studentportal/src/App.js
@@ -8,6 +8,7 @@ import Error from "./components/Error/Error";
 import Blog from "./components/Blog/Blog";
 import BranchSection from "./components/BranchSection/BranchSection";
 import Documentation from "./components/Documentation/Documentation";
+import branches from "./data/branches.json";
 
 function App() {
   return (
@@ -18,62 +19,15 @@ function App() {
           <Switch>
             <Route exact path="/blog" component={Blog} />
             <Route exact path="/" component={Home} />
-            <Route
-              exact
-              path="/web-development"
-              component={() => <BranchSection 
-                                        name="Web Development" 
-                                        intro = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tempor massa. Praesent at lacinia nisi, faucibus finibus mauris." />}
-            />
-            <Route
-              exact
-              path="/app-development"
-              component={() => <BranchSection 
-                                        name="App Development"
-                                        intro = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tempor massa. Praesent at lacinia nisi, faucibus finibus mauris." />}
-            />
-            <Route
-              exact
-              path="/robotics"
-              component={() => <BranchSection 
-                                        name="Robotics and IoT"
-                                        intro = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tempor massa. Praesent at lacinia nisi, faucibus finibus mauris." />}
-            />
-            <Route
-              exact
-              path="/machine-learning"
-              component={() => <BranchSection name="Machine Learning" 
-                                        intro = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tempor massa. Praesent at lacinia nisi, faucibus finibus mauris."/>}
-            />
-            <Route
-              exact
-              path="/competitive-programming"
-              component={() => <BranchSection name="Competitive Programming" 
-                                        intro = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tempor massa. Praesent at lacinia nisi, faucibus finibus mauris."/>}
-            />
-            <Route
-              exact
-              path="/ar-vr"
-              component={() => <BranchSection name="AR/VR" 
-                                        intro = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tempor massa. Praesent at lacinia nisi, faucibus finibus mauris."/>}
-            />
-            <Route
-              exact
-              path="/3d-modelling"
-              component={() => <BranchSection name="3D Modelling" 
-                                        intro = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tempor massa. Praesent at lacinia nisi, faucibus finibus mauris."/>}
-            />
-            <Route
-              exact
-              path="/cloud-computing"
-              component={() => <BranchSection name="Cloud Computing" 
-                                        intro = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tempor massa. Praesent at lacinia nisi, faucibus finibus mauris."/>}
-            />
+            {
+              branches.map((obj)=>{
+                return <Route exact path={obj.link} component={()=> <BranchSection name={obj.name} intro={obj.intro}/>} />
+              })
+            }
             <Route
               exact
               path="/terms-and-policy"
-              component={() => <Documentation name="Terms and Policy" 
-                                        intro = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tempor massa. Praesent at lacinia nisi, faucibus finibus mauris."/>}
+              component={() => <Documentation name="Terms and Policy"/>}
             />
             <Route path="/" component={Error} />
           </Switch>
