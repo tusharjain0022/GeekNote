9c65830fe3c6326b35a4b4140ef7bbd897607719
sandesh-08 <67503294+sandesh-08@users.noreply.github.com>
2021-02-03 20:08:28 +0530

    Merging multiple json's  (#140)
    * deleted extra json files
    
    * merging json's
    
    
    * slight  change
    
    Co-authored-by: Abhay Raj <abhayrajsingh1707@gmail.com>

--- studentportal/src/components/Home/Contact/DevModal/DevModal.js
+++ studentportal/src/components/Home/Contact/DevModal/DevModal.js
@@ -3,7 +3,7 @@ import github from "../../../../images/GitHub_logo.svg";
 import instagram from "../../../../images/Instagram_logo.svg";
 import linkedin from "../../../../images/LinkedIn_logo.svg";
 import "./DevModal.css";
-import developers from "./DevDetails";
+import dev_adm_contri from "../../../../data/dev_adm_contri.json";
 
 function DevModal() {
   return (
@@ -32,47 +32,56 @@ function DevModal() {
                 style={{ borderTop: "1px solid   #ccd6f6", width: "70%" }}
               ></hr>
             </div>
-            {developers.map((developer, idx) => {
-              return (
-                <div className="col-12 px-5 my-3" key={`${idx}`}>
-                  <div className="row">
-                    <img
-                      className={
-                        developer.id % 2 === 0
-                          ? "col-12 col-lg-4  align-self-center dev-images"
-                          : "col-12 col-lg-4  align-self-center dev-images order-lg-2"
-                      }
-                      src={developer.image}
-                      alt="tushar"
-                    ></img>
-                    <div
-                      className={
-                        developer.id % 2 === 0
-                          ? "dev-text-box col-12 col-lg-8"
-                          : "dev-text-box col-12 col-lg-8 order-lg-1"
-                      }
-                    >
-                      <h4 className="mt-3">{developer.name}</h4>
-                      <p className="dev-intro"> {developer.intro}</p>
-                      <div className="Links">
-                        <a href={developer.githubLink} className="mr-3">
-                          <img src={github} alt="github" />
-                        </a>
-                        <a href={developer.linkedinLink} className="mr-3">
-                          <img src={linkedin} alt="Dribbble" />
-                        </a>
-                        <a href={"mailto:" + developer.mail} className="mr-3">
-                          <img src={mail} alt="mail" />
-                        </a>
-                        <a href={developer.instaLink} className="mr-3">
-                          <img src={instagram} alt="instagram" />
-                        </a>
+
+            {/* rendering developer's here */}
+            {dev_adm_contri.map((dev_info, idx) => {
+              if(dev_info.developer===true)
+              {
+                return (
+                  <div className="col-12 px-5 my-3" key={`${idx}`}>
+                    <div className="row">
+                      <img
+                        className={
+                          dev_info.id % 2 === 0
+                            ? "col-12 col-lg-4  align-self-center dev-images"
+                            : "col-12 col-lg-4  align-self-center dev-images order-lg-2"
+                        }
+                        src={dev_info.image}
+                        alt="tushar"
+                      ></img>
+                      <div
+                        className={
+                          dev_info.id % 2 === 0
+                            ? "dev-text-box col-12 col-lg-8"
+                            : "dev-text-box col-12 col-lg-8 order-lg-1"
+                        }
+                      >
+                        <h4 className="mt-3">{dev_info.name}</h4>
+                        <p className="dev-intro"> {dev_info.intro}</p>
+                        <div className="Links">
+                          <a href={dev_info.githubLink} className="mr-3">
+                            <img src={github} alt="github" />
+                          </a>
+                          <a href={dev_info.linkedinLink} className="mr-3">
+                            <img src={linkedin} alt="Dribbble" />
+                          </a>
+                          <a href={"mailto:" + dev_info.mail} className="mr-3">
+                            <img src={mail} alt="mail" />
+                          </a>
+                          <a href={dev_info.instaLink} className="mr-3">
+                            <img src={instagram} alt="instagram" />
+                          </a>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              );
+                );
+              }
+              else{
+                return null;
+              }
             })}
+            {/* function ends here */}
 
             <div className="col-12 mt-5">
               <div className="text-center text-white">
