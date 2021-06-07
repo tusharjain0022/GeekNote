398c3a87ea1b383ea0d9b1129e6fd3508290a9b4
Sahil Gupta <sahilgupta.btech.cs19@iiitranchi.ac.in>
2021-06-07 10:35:51 +0530

    Error Handling in API completed (#158)
    * Filtering, Sorting and Pagination done for Blogs
    
    * Refactoring Code
    
    * Validators and Debugger dependency added
    
    * Error Handling for API
    
    * small update

--- studentportal-backend/server.js
+++ studentportal-backend/server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('Uncaught Exception! Shutting Down...........😢😢');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -19,6 +25,14 @@ mongoose
   .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}....`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('Unhandled Rejection! Shutting Down...........😢😢');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
