2e3b7621d996707f1b211732e9c6861d3f93dd2d
Sahil Gupta <sahilgupta.btech.cs19@iiitranchi.ac.in>
2021-07-07 13:58:56 +0530

    fixing Deployement Errors

--- studentportal-backend/app.js
+++ studentportal-backend/app.js
@@ -28,6 +28,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('./../studentportal/build'));
   app.use((req, res, next) => {
     res.sendFile('./../studentportal/build/index.html');
+    next();
   });
 }
 
