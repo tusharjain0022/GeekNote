98ce9612cfe0cbca3c0104bfa810d3f2d6bd460f
Sahil Gupta <sahilgupta.btech.cs19@iiitranchi.ac.in>
2021-07-07 13:13:52 +0530

    Changes for deployement done

--- studentportal-backend/app.js
+++ studentportal-backend/app.js
@@ -23,6 +23,10 @@ app.use((req, res, next) => {
   next();
 });
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('./../studentportal/build'));
+}
+
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.setHeader(
